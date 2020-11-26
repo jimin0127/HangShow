25c0e5f7be7ccfb3d72f67b63ad488297ae0a100
jimin0127 <ckdgh4662@naver.com>
2020-11-27 01:43:11 +0900

    models.py

--- HS/admin.py
+++ HS/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 # Register your models here.
+class UserAdmin(BaseUserAdmin):
+    @admin.register(User)
+    class UserAdmin(admin.ModelAdmin):
+        list_display = (
+            'id',
+            'name',
+            'student_id',
+        )
+
+        list_display_links = (
+            'id',
+        )
--- HangShow/settings.py
+++ HangShow/settings.py
@@ -27,10 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+AUTH_USER_MODEL = 'HS.User'
 # Application definition
 
 INSTALLED_APPS = [
+    'HS',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
