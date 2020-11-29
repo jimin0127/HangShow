01b37def46a4e46364ed6f1d43611ea766f62a58
jimin0127 <ckdgh4662@naver.com>
2020-11-30 01:27:52 +0900

    join 구현 중

---
+++ HS/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import User
+from django.utils.translation import ugettext_lazy as _
+
+
+class UserCreationForm(forms.ModelForm):
+    id = forms.CharField()
+    name = forms.CharField()
+    student_id = forms.CharField(max_length=4)
+    email = forms.EmailField()
+    password = forms.PasswordInput()
+
+    class Meta:
+        model = User
+        fields = ['id', 'name', 'student_id', 'email']
