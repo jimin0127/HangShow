0524fda032785765e6136242c5b0a1ffb6c171f2
guddus326 <guddus326@naver.com>
2020-12-01 05:34:50 +0900

    네비바 정리

--- HS/urls.py
+++ HS/urls.py
@@ -2,12 +2,11 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-
     path('assignment/', views.getAssignment, name='assignment'),
     path('calendar/',views.calendar, name='calendar'),
+    path('listview/',views.AssignmentList.as_view(), name='listview'),
 
     path('',views.index),
-
 ]
 
 
